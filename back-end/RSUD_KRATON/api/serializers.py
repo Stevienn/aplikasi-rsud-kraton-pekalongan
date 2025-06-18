21e7cabf9ac221ba5f75d0662d842e6c9178062b
putri <bangtanviarta@gmail.com>
2025-06-18 21:09:11 +0700

    feat: tambah spesialisasi dan ulang isi db

--- back-end/RSUD_KRATON/api/serializers.py
+++ back-end/RSUD_KRATON/api/serializers.py
@@ -1,11 +1,48 @@
 from rest_framework import serializers
 from ..models import *
 
+class SpesialisasiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Spesialisasi
+        fields = '__all__'
+
+class DokterSerializer(serializers.ModelSerializer):
+    # spesialisasi = SpesialisasiSerializer(read_only=True, source='spesialisasi_dokter')
+    # spesialisasi_id = serializers.PrimaryKeyRelatedField(
+    #       queryset = Spesialisasi.objects.all(), source='spesialisasi', write_only=True
+    # )
+    class Meta:
+        model = Dokter
+        fields = '__all__'
+
+class IcdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ICD
+        fields = '__all__'
+
+class HistorySerializer(serializers.ModelSerializer):
+    data_dokter = DokterSerializer(read_only=True, source='Dokter')
+    data_dokter_id = serializers.PrimaryKeyRelatedField(
+        queryset=Dokter.objects.all(), write_only=True, source='Dokter', required=False, allow_null=True
+    )
+    diagnosa_primary = IcdSerializer(read_only=True)
+    diagnosa_primary_id = serializers.PrimaryKeyRelatedField(
+        queryset=ICD.objects.all(), write_only=True, source='diagnosa_primary'
+    )
+    diagnosa_secondary = IcdSerializer(read_only=True)
+    diagnosa_secondary_id = serializers.PrimaryKeyRelatedField(
+        queryset=ICD.objects.all(), write_only=True, source='diagnosa_secondary', required=False, allow_null=True
+    )
+    class Meta:
+        model = History
+        fields = ['id', 'data_dokter', 'data_dokter_id', 'tanggal_konsultasi', 'keluhan', 'diagnosa_sub', 'diagnosa_primary', 'diagnosa_primary_id', 'diagnosa_secondary', 'diagnosa_secondary_id']
+
 class PasienSerializer(serializers.ModelSerializer):
     tanggal_lahir = serializers.DateField(
     format="%d-%m-%Y",
     input_formats=["%d-%m-%Y"]
 )
+    rekap_medis = HistorySerializer(many=True, read_only=True)
     class Meta: 
         model = Pasien
         fields = '__all__'
@@ -31,77 +68,16 @@ class HariPraktekSerializer(serializers.ModelSerializer):
         model = hari_praktek
         fields = ['id', 'hari']
 
-class DokterSerializer(serializers.ModelSerializer):
-    # schedule_dokter = HariPraktekSerializer(many=True, read_only=True)
-    class Meta:
-        model = Dokter
-        fields = '__all__'
-
-# class DokterWithoutScheduleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Dokter
-#         exclude = ['schedule_dokter', 'password_dokter']
-
-class DokterSpesialisSerializer(serializers.ModelSerializer):
-    # schedule_dokter = HariPraktekSerializer(many=True, read_only=True)
-    class Meta:
-        model = Dokter_spesialis
-        fields = '__all__'
-        
-# class DokterSpesialisWithoutScheduleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Dokter_spesialis
-#         exclude = ['schedule_dokter', 'password_dokter']
-
-class IcdSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ICD
-        fields = '__all__'
-
 class PerawatSerializer(serializers.ModelSerializer):
     class Meta:
         model = perawat
         fields = '__all__'
-
-class HistorySerializer(serializers.ModelSerializer):
-    data_dokter_umum = DokterSerializer(read_only=True, source='Dokter')
-    data_dokter_umum_id = serializers.PrimaryKeyRelatedField(
-        queryset=Dokter.objects.all(), write_only=True, source='Dokter', required=False, allow_null=True
-    )
-    data_dokter_spesialis = DokterSpesialisSerializer(read_only=True, source='Dokter_spesialis')
-    data_dokter_spesialis_id = serializers.PrimaryKeyRelatedField(
-        queryset=Dokter_spesialis.objects.all(), write_only=True, source='Dokter_spesialis', required=False, allow_null=True
-    )
-    diagnosa_primary = IcdSerializer(read_only=True)
-    diagnosa_primary_id = serializers.PrimaryKeyRelatedField(
-        queryset=ICD.objects.all(), write_only=True, source='diagnosa_primary'
-    )
-    diagnosa_secondary = IcdSerializer(read_only=True)
-    diagnosa_secondary_id = serializers.PrimaryKeyRelatedField(
-        queryset=ICD.objects.all(), write_only=True, source='diagnosa_secondary', required=False, allow_null=True
-    )
-    class Meta:
-        model = History
-        fields = ['id', 'data_dokter_umum', 'data_dokter_umum_id', 'data_dokter_spesialis', 'data_dokter_spesialis_id', 'tanggal_konsultasi', 'keluhan', 'diagnosa_sub', 'diagnosa_primary', 'diagnosa_primary_id', 'diagnosa_secondary', 'diagnosa_secondary_id']
-
-class RekapMedisSerializer(serializers.ModelSerializer):
-    data_pasien = PasienSerializer(read_only=True, source='ID_BPJS')  # untuk GET (nested)
-    data_pasien_id = serializers.PrimaryKeyRelatedField(
-        queryset=Pasien.objects.all(), write_only=True, source='ID_BPJS'
-    ) 
-    history = HistorySerializer(many=True, read_only=True)
-    history_ids = serializers.PrimaryKeyRelatedField(queryset=History.objects.all(), write_only=True, many=True, source='history')
-
-    class Meta:
-        model = rekap_medis
-        fields = ['data_pasien', 'data_pasien_id', 'history', 'history_ids']
 
 class SchedulePraktekSerializer(serializers.ModelSerializer):
     data_pendaftaran_ids = serializers.PrimaryKeyRelatedField(queryset=Pendaftaran.objects.all(), write_only=True, many=True, source='data_pendaftaran')
     data_pendaftaran = PendaftaranSerializer(many=True, read_only=True)
     hari = HariPraktekSerializer(read_only=True)
     dokter_umum = DokterSerializer(read_only=True)
-    dokter_spesialis = DokterSpesialisSerializer(read_only=True)
     
     class Meta:
         model = schedule_praktek
--- back-end/RSUD_KRATON/models.py
+++ back-end/RSUD_KRATON/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 
 #Create your models here.,
+class Spesialisasi(models.Model):
+    nama_spesialisasi = models.CharField(max_length=100)
+
+    def __str__(self):
+        return (f"Id Spesialisasi : {self.id}, Spesialisasi  : {self.nama_spesialisasi}")
+
+class Dokter(models.Model):
+    nama_dokter = models.CharField(max_length=50)
+    spesialisasi_dokter = models.ForeignKey(Spesialisasi, on_delete=models.CASCADE, related_name='spesialisasi_dokter_set', blank='True')
+    password_dokter = models.CharField(max_length=20)
+    email_dokter = models.CharField(max_length=50)
+    image_dokter = models.CharField(max_length=100)
+
+    def __str__(self):
+        return (f"Id Dokter : {self.id}, Nama Dokter : {self.nama_dokter}")
+    
 class ICD(models.Model):
     kode = models.CharField(max_length=20)
     nama_diagnosa = models.CharField(max_length=100)
 
     def __str__(self):
         return (f'id : {self.id}, Kode : {self.kode}, Diagnosa : {self.nama_diagnosa}')
+
+class History(models.Model):
+    tanggal_konsultasi = models.DateField()
+    keluhan = models.CharField(max_length=100)
+    diagnosa_sub = models.CharField(max_length=200)
+    diagnosa_primary = models.ForeignKey(ICD, on_delete=models.CASCADE, related_name='diagnosa_primary_set')
+    diagnosa_secondary = models.ForeignKey(ICD, on_delete=models.CASCADE, related_name='diagnosa_secondary_set', blank=True, null=True)
+    Dokter = models.ForeignKey(Dokter, on_delete=models.CASCADE, related_name='dokter_umum_set', blank=True, null=True)
     
 class Pasien(models.Model):
     ID_BPJS = models.CharField(max_length=13,primary_key=True)
@@ -16,6 +40,7 @@ class Pasien(models.Model):
     nomor_HP = models.CharField(max_length=13)
     email_pasien = models.CharField(max_length=50)
     nomor_urut = models.IntegerField(null=True, blank=True)
+    rekap_medis = models.ManyToManyField(History, related_name='rekap_medis_set', blank=True)
 
     def __str__(self):
         return (f"BPJS: {self.ID_BPJS}, Nama: {self.nama}, Jenis Kelamin: {self.jenis_kelamin}, TTL: {self.tanggal_lahir}, HP: {self.nomor_HP}, email: {self.email_pasien}")
@@ -35,39 +60,14 @@ class hari_praktek(models.Model):
 
     def __str__(self):
         return (f"ID : {self.id}, Hari : {self.hari}")
-
-class Dokter(models.Model):
-    nama_dokter = models.CharField(max_length=50)
-    password_dokter = models.CharField(max_length=20)
-    email_dokter = models.CharField(max_length=50)
-    image_dokter = models.CharField(max_length=100)
-    # schedule_dokter = models.ManyToManyField(hari_praktek, related_name='hari_praktek_dokter_set')
-
-    def __str__(self):
-        return (f"Id Dokter : {self.id}, Nama Dokter : {self.nama_dokter}")
-
-class Dokter_spesialis(models.Model):
-    nama_dokter = models.CharField(max_length=50)
-    password_dokter = models.CharField(max_length=20)
-    email_dokter = models.CharField(max_length=50)
-    spesialization = models.CharField(max_length=100)
-    image_dokter = models.CharField(max_length=100)
-    # schedule_dokter = models.ManyToManyField(hari_praktek, related_name='hari_praktek_dokter_spc_set')
-
-    def __str__(self):
-        return (f"Id Dokter : {self.id}, Nama Dokter : {self.nama_dokter}, Spesialis  : {self.spesialization}")
     
 class schedule_praktek(models.Model):
     dokter_umum = models.ForeignKey(Dokter, blank=True, on_delete=models.CASCADE, null=True, related_name="dokter_umum_praktek_set")
-    dokter_spesialis = models.ForeignKey(Dokter_spesialis, blank=True, on_delete=models.CASCADE, null=True, related_name="dokter_spesialis_praktek_set")
     data_pendaftaran = models.ManyToManyField(Pendaftaran, related_name="data_pendaftaran_set", blank=True)
     hari = models.ForeignKey(hari_praktek, on_delete=models.CASCADE, related_name="hari_praktek_set")
     jam_mulai = models.CharField(max_length=100)
     jam_selesai = models.CharField(max_length=100)
     
-    # def __str__(self):
-    #     dokter = self.dokter_umum if self.dokter_umum else self.dokter_spesialis
-    #     return (f"Id Praktek : {self.id}, Nama Dokter : {dokter}, Hari :{self.hari}")
     
 class perawat(models.Model):
     #id_perawat = models.IntegerField()
@@ -77,19 +77,3 @@ class perawat(models.Model):
 
     def __str__(self):
         return (f"Perawat : {self.nama_perawat}")
-
-class History(models.Model):
-    tanggal_konsultasi = models.DateField()
-    keluhan = models.CharField(max_length=100)
-    diagnosa_sub = models.CharField(max_length=200)
-    diagnosa_primary = models.ForeignKey(ICD, on_delete=models.CASCADE, related_name='diagnosa_primary_set')
-    diagnosa_secondary = models.ForeignKey(ICD, on_delete=models.CASCADE, related_name='diagnosa_secondary_set', blank=True, null=True)
-    Dokter = models.ForeignKey(Dokter, on_delete=models.CASCADE, related_name='dokter_umum_set', blank=True, null=True)
-    Dokter_spesialis = models.ForeignKey(Dokter_spesialis, on_delete=models.CASCADE, related_name='dokter_spesialis_set', blank=True, null=True)
-
-class rekap_medis(models.Model):
-    ID_BPJS = models.ForeignKey(Pasien, primary_key=True, on_delete=models.CASCADE, related_name='pasien_set')
-    history = models.ManyToManyField(History, related_name='history_set')
-
-    def __str__(self):
-        return (f'id : {self.ID_BPJS}')
